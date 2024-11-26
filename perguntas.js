dedb04d9c34f8bdf5256a298b32919a72bb66b82
Leonardo <leonardo.dellamatrice@gmail.com>
2024-11-26 15:22:04 -0300

    final (teorico) 1

--- perguntas.js
+++ perguntas.js
@@ -242,19 +242,24 @@ function verificarResposta() {
         // Verifica se a resposta está correta
         const correto = respostaSelecionada === pergunta.respostaCorreta;
         if (correto) {
-            if (indicePerguntaAtual === 0){
-                acertou(`natalecotec/ligar/faixa/blue`)
-            } else if (indicePerguntaAtual === 1){
-                acertou(`natalecotec/ligar/faixa/red`)
-            } else if (indicePerguntaAtual === 2){
-                acertou(`natalecotec/ligar/faixa/green`)
-            } else if (indicePerguntaAtual === 3){} {
-                acertou(`natalecotec/ligar/faixa/yellow`)
-            }
-
             respostasCorretas++;
             document.getElementById('result').textContent = 'Resposta Correta!';
             document.querySelector(`label[for="option${respostaSelecionada}"]`).style.backgroundColor = 'green';
+            console.log(indicePerguntaAtual)
+
+            if (indicePerguntaAtual === 0){
+                acertou(`natalecotec/ligar/faixa/blue`)
+                console.log("azul")
+            } else if (indicePerguntaAtual === 1){
+                acertou(`natalecotec/ligar/faixa/red`)
+                console.log("vermelho")
+            } else if (indicePerguntaAtual === 2){
+                acertou(`natalecotec/ligar/faixa/green`)
+                console.log("verde")
+            } else if (indicePerguntaAtual === 3){} {
+                acertou(`natalecotec/ligar/faixa/yellow`)
+                console.log("amarelo")
+            }
         } else {
             document.getElementById('result').textContent = 'Resposta Errada!';
             document.querySelector(`label[for="option${respostaSelecionada}"]`).style.backgroundColor = 'red';
